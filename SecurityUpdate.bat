3dabe87606308352d1521d429ee1423243318194
ojaspurwar <ojaspurwar@gmail.com>
2026-02-02 23:13:12 +0530

    Virus

---
+++ SecurityUpdate.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0a
+title SYSTEM CRITICAL ERROR
+echo WARNING: VIRUS DETECTED IN C:/WINDOWS/SYSTEM32
+pause
+echo INITIATING HARD DRIVE WIPE...
+timeout /t 2 >nul
+:matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%
+goto matrix
